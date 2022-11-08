8ade656e6f6088e5403c450d19d06068baa7b01d
Auryn Macmillan <hi@auryn.xyz>
2022-11-08 22:52:26 +0100

    use safeTransferFrom()

--- contracts/ConnextModule.sol
+++ contracts/ConnextModule.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.15;
 
 import "@gnosis.pm/zodiac/contracts/core/Module.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IXReceiver} from "./interfaces/IXReceiver.sol";
 
 contract ConnextModule is Module, IXReceiver {
